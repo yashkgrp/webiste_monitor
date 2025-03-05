e866128902480e6b550370593c097e79145c7b40
yashfinkraft <yash@kgrp.in>
2025-03-05 16:24:05 +0530

    update the base url to production

--- website-monitor-react/src/config.js
+++ website-monitor-react/src/config.js
@@ -1,6 +1,6 @@
 // API Configuration
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || "https://localhost:5000";
+  process.env.REACT_APP_API_BASE_URL || "https://monitor.finkraftai.com/";
 
 // Socket Configuration
 export const SOCKET_CONFIG = {
